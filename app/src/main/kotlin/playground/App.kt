5f93589b38e8dba48a4169d7abb969e03688aca7
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-01-13 07:17:02 -0300

    Orientação a Objetos (OO) Visando Soluções Mais Idiomáticas

--- app/src/main/kotlin/playground/App.kt
+++ app/src/main/kotlin/playground/App.kt
@@ -3,49 +3,42 @@ package playground
 /*
 
 Desafio
-Faça um programa que calcule e imprima o salário a ser transferido para um funcionário. Para realizar o calculo, primeiro receba o valor do salário bruto (valorSalario) e adicional dos benefícios (valorBeneficios). Por fim, o salário a ser transferido é calculado da seguinte maneira:
+Desenvolver um programa que determine em quantos anos a população do país A ultrapassará ou igualará a população do país B. Para isso, considere as seguintes informações:
 
-(valorSalario - valorImpostos) + valorBeneficios
-
-Para calcular o valorImpostos, seguem as aliquotas (baseadas no valor do salário bruto):
-
-    De R$ 0.00 a R$ 1100.00 = 5.00%
-    De R$ 1100.01 a R$ 2500.00 = 10.00%
-    Maior que R$ 2500.00 = 15.00%
+População Inicial: País A tem N habitantes e País B tem M habitantes, onde N é sempre menor que M.
+Taxas de Crescimento Anual: País A cresce a uma taxa de 3% ao ano, enquanto País B cresce a uma taxa de 1.5% ao ano.
+Cálculo Requerido: O programa deve calcular o número de anos necessários para que a população de A seja igual ou maior que a de B, considerando as taxas de crescimento contínuas.
+Observação: As taxas de crescimento devem ser aplicadas anualmente a partir das populações iniciais.
 
 Entrada
-A entrada consiste em vários arquivos de teste, que conterá o valor do salário bruto (valorSalario) e adicional dos benefícios (valorBeneficios). Conforme mostrado no exemplo de entrada a seguir.
+A entrada consiste em dois valores inteiros, sendo N a população do país A e M a população do país B.
 
 Saída
-Para cada conjunto de Entradas (valorSalario e valorBeneficios), deverá ser calculada uma Saída. Para isso, basta imprimir uma linha com o resultado da seguinte fórmula:
-
-saida = (valorSalario - valorImpostos) + valorBeneficios
-
-Use o exemplo abaixo para entender melhor a formatação das Entradas e Saída.
+A saída consiste em retornar o número de anos necessários para que a população do país A ultrapasse ou iguale a população B, mantidas as taxas de crescimento. Confira exemplo abaixo:
 
 Exemplos
 A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
 
 */
 
-object ReceitaFederal {
-    fun calcularImposto(salario: Double): Double {
-        val aliquota = when {
-            (salario in 0.0..1100.0) -> 0.05
-            (salario > 1100 && salario <= 2200) -> 0.10
-            (salario > 2200) -> 0.15
-            else -> throw IllegalArgumentException("Salário não pode ser negativo.")
-        }
-        return aliquota * salario
+data class Pais(var habitantes: Double, val taxaCrescimento: Double) {
+    fun crescerPopulacaoAnual() {
+        habitantes *= (1 + taxaCrescimento)
     }
 }
 
 fun main() {
-    val valorSalario = readln().toDouble()
-    val valorBeneficios = readln().toDouble()
+    val habitantesPaisA = readln().toDouble()
+    val habitantesPaisB = readln().toDouble()
+    val paisA = Pais(habitantesPaisA, taxaCrescimento = 0.03)
+    val paisB = Pais(habitantesPaisB, taxaCrescimento = 0.015)
 
-    val valorImposto = ReceitaFederal.calcularImposto(valorSalario)
-    val saida = valorSalario - valorImposto + valorBeneficios
+    var quantidadeAnos = 0
+    while (paisA.habitantes < paisB.habitantes) {
+        paisA.crescerPopulacaoAnual()
+        paisB.crescerPopulacaoAnual()
+        quantidadeAnos++
+    }
 
-    println(String.format("%.2f", saida))
+    println("$quantidadeAnos anos")
 }
