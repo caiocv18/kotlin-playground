5866a1c9d567368214ec8476932f7797edc44197
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-01-17 18:07:36 -0300

    Singleton

--- app/src/main/kotlin/playground/App.kt
+++ app/src/main/kotlin/playground/App.kt
@@ -2,43 +2,86 @@ package playground
 
 /*
 
-Desafio
-Desenvolver um programa que determine em quantos anos a população do país A ultrapassará ou igualará a população do país B. Para isso, considere as seguintes informações:
+Descrição
+O Singleton é uma abordagem de design de software que visa assegurar a existência de apenas uma instância de uma classe e fornecer um ponto centralizado para acessá-la. Isso é especialmente benéfico em contextos nos quais é desejável manter uma única ocorrência de uma classe responsável pelo controle de um recurso compartilhado, como configurações, conexões de banco de dados ou caches.
 
-População Inicial: País A tem N habitantes e País B tem M habitantes, onde N é sempre menor que M.
-Taxas de Crescimento Anual: País A cresce a uma taxa de 3% ao ano, enquanto País B cresce a uma taxa de 1.5% ao ano.
-Cálculo Requerido: O programa deve calcular o número de anos necessários para que a população de A seja igual ou maior que a de B, considerando as taxas de crescimento contínuas.
-Observação: As taxas de crescimento devem ser aplicadas anualmente a partir das populações iniciais.
+Neste desafio, você deve criar um sistema de gerenciamento de usuários que permita adicionar e listar usuários. Você tem a opção de implementar o padrão Singleton para garantir que haja apenas uma instância do gerenciador de usuários em toda a aplicação. No entanto, a implementação do padrão Singleton é opcional e você pode optar por seguir uma abordagem diferente para resolver o desafio, se preferir.
 
+Especificações do Desafio:
+
+Crie uma classe User com os seguintes atributos: id (inteiro) e name (string).
+Implemente uma classe UserManager que siga o padrão Singleton. Esta classe deve possuir as seguintes funcionalidades:
+a. Adicionar um novo usuário ao sistema, recebendo o nome como entrada.
+b. Listar todos os usuários cadastrados.
+No programa principal (main), siga as etapas abaixo:
+a. Solicite ao usuário a quantidade de usuários que deseja cadastrar.
+b. Peça ao usuário para informar os nomes dos usuários, um por linha.
+c. Após receber os nomes e cadastrar os usuários, liste os usuários cadastrados.
 Entrada
-A entrada consiste em dois valores inteiros, sendo N a população do país A e M a população do país B.
+Um número inteiro representando a quantidade de usuários que o usuário deseja cadastrar.
+
+Para cada usuário a ser cadastrado, uma string contendo o nome do usuário.
 
 Saída
-A saída consiste em retornar o número de anos necessários para que a população do país A ultrapasse ou iguale a população B, mantidas as taxas de crescimento. Confira exemplo abaixo:
+Uma lista com os nomes dos usuários cadastrados.Descrição
+O Singleton é uma abordagem de design de software que visa assegurar a existência de apenas uma instância de uma classe e fornecer um ponto centralizado para acessá-la. Isso é especialmente benéfico em contextos nos quais é desejável manter uma única ocorrência de uma classe responsável pelo controle de um recurso compartilhado, como configurações, conexões de banco de dados ou caches.
 
-Exemplos
-A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+Neste desafio, você deve criar um sistema de gerenciamento de usuários que permita adicionar e listar usuários. Você tem a opção de implementar o padrão Singleton para garantir que haja apenas uma instância do gerenciador de usuários em toda a aplicação. No entanto, a implementação do padrão Singleton é opcional e você pode optar por seguir uma abordagem diferente para resolver o desafio, se preferir.
+
+Especificações do Desafio:
+
+Crie uma classe User com os seguintes atributos: id (inteiro) e name (string).
+Implemente uma classe UserManager que siga o padrão Singleton. Esta classe deve possuir as seguintes funcionalidades:
+a. Adicionar um novo usuário ao sistema, recebendo o nome como entrada.
+b. Listar todos os usuários cadastrados.
+No programa principal (main), siga as etapas abaixo:
+a. Solicite ao usuário a quantidade de usuários que deseja cadastrar.
+b. Peça ao usuário para informar os nomes dos usuários, um por linha.
+c. Após receber os nomes e cadastrar os usuários, liste os usuários cadastrados.
+Entrada
+Um número inteiro representando a quantidade de usuários que o usuário deseja cadastrar.
+
+Para cada usuário a ser cadastrado, uma string contendo o nome do usuário.
+
+Saída
+Uma lista com os nomes dos usuários cadastrados.
+
+Nota:
+A implementação do padrão Singleton traz uma abordagem centralizada, com uma única instância do gerenciador de entidades, favorecendo a consistência dos dados e facilitando o acesso global. Por outro lado, optar por não usar o padrão permite maior flexibilidade, possibilitando várias instâncias independentes. A escolha depende das demandas do projeto, design e manutenção. Isso garante adaptabilidade entre diferentes linguagens ou contextos.
+
+Caso queira saber mais sobre o Design Pattern Singleton:
+https://refactoring.guru/pt-br/design-patterns/singleton
 
 */
 
-data class Pais(var habitantes: Double, val taxaCrescimento: Double) {
-    fun crescerPopulacaoAnual() {
-        habitantes *= (1 + taxaCrescimento)
+class User(val id: Int, val name: String)
+
+/*
+ * No Kotlin, a declaração de um objeto (por meio da palavra-chave object)
+ * é uma maneira concisa e eficaz de implementar o padrão Singleton.
+ */
+object UserManager {
+
+    private val users = mutableListOf<User>()
+
+    fun addUser(id: Int, name: String) {
+        users.add(User(id, name))
+    }
+
+    fun listUsers() {
+        users.forEach { user ->
+            println("${user.id} - ${user.name}")
+        }
     }
 }
 
 fun main() {
-    val habitantesPaisA = readln().toDouble()
-    val habitantesPaisB = readln().toDouble()
-    val paisA = Pais(habitantesPaisA, taxaCrescimento = 0.03)
-    val paisB = Pais(habitantesPaisB, taxaCrescimento = 0.015)
+    val quantity = readlnOrNull()?.toIntOrNull() ?: 0
 
-    var quantidadeAnos = 0
-    while (paisA.habitantes < paisB.habitantes) {
-        paisA.crescerPopulacaoAnual()
-        paisB.crescerPopulacaoAnual()
-        quantidadeAnos++
+    for (i in 1..quantity) {
+        val name = readlnOrNull() ?: ""
+        UserManager.addUser(i, name)
     }
 
-    println("$quantidadeAnos anos")
+    UserManager.listUsers()
 }
